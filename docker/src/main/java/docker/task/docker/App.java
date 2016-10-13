2ec46acb37573906a69f978d1e023fd3b852cef4
uc209259 <uc209259@UC209259-TPD-A.ten.thomsonreuters.com>
2016-10-13 09:54:36 -0400

    Initial checkin

---
+++ docker/src/main/java/docker/task/docker/App.java
@@ -0,0 +1,13 @@
+package docker.task.docker;
+
+/**
+ * @uc209259
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "*****Task for deployment Engineer position*****" );
+    }
+}
