c7ca48453ecd155bf7d1998267842822d1e4fc4f
uc209259 <uc209259@UC209259-TPD-A.ten.thomsonreuters.com>
2016-10-13 11:23:54 -0400

    Read and display contents of a file

--- docker/src/main/java/docker/task/docker/App.java
+++ docker/src/main/java/docker/task/docker/App.java
@@ -1,13 +1,39 @@
 package docker.task.docker;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 /**
  * @uc209259
  *
  */
 public class App 
 {
-    public static void main( String[] args )
+    public static void main( String[] args ) throws IOException
     {
         System.out.println( "*****Task for deployment Engineer position*****" );
+        
+        System.out.println("FilePath: "+args[0]);
+        
+        File file = new File(args[0]);
+        
+        if(file.exists()){
+			
+			System.out.println("File exists");
+			FileReader reader = new FileReader(file);
+			StringBuilder fileContent = new StringBuilder();
+			BufferedReader br = new BufferedReader(reader);
+			String line = br.readLine();
+			while(line!="End"){
+				fileContent = fileContent.append(line);
+				fileContent.append(System.lineSeparator());
+				line = br.readLine();
+			}
+			br.close();
+			System.out.println(fileContent);
+		}
     }
 }
